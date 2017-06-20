fa539002d58880affdfcdf17d2f681ef1fde2557
Teofilo Cerda <tcerdaj@hotmail.com.com>
2017-06-20 09:16:02 -0400

    Adding value injecter.

---
+++ JehovaJireh.Infrastructure/Installers/PersistenceInstaller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using JehovaJireh.Infrastructure.Plumbing;
+
+namespace JehovaJireh.Infrastructure.Installers
+{
+	public class PersistenceInstaller : IWindsorInstaller
+	{
+		public void Install(IWindsorContainer container, IConfigurationStore store)
+		{
+			container.AddFacility<PersistenceFacility>();
+		}
+	}
+}
