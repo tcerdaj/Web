555d3d16b1c6032685f3183aea9779905e5f564f
Teofilo Cerda <tcerdaj@hotmail.com.com>
2017-11-28 22:56:08 -0500

    adding scheduler class

---
+++ JehovaJireh.Core/Entities/Scheduler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JehovaJireh.Core.Entities
+{
+    public class Scheduler:EntityBase<int>
+    {
+        public virtual Donation Donation { get; set; }
+        public virtual Guid ItemId { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string ImageUrl { get; set; }
+        public virtual string Description { get; set; }
+        public virtual DateTime? StartDate { get; set; }
+        public virtual DateTime? EndDate { get; set; }
+        public virtual string RecurrenceId { get; set; }
+        public virtual string RecurrenceException { get; set; }
+        public virtual string RecurrenceRule { get; set; }
+        public virtual bool IsAllDay { get; set; }
+
+    }
+}
---
+++ JehovaJireh.Core/EntitiesDto/SchedulerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JehovaJireh.Core.EntitiesDto
+{
+    public class SchedulerDto:EntityBaseDto<int>
+    {
+        public virtual DonationDto Donation { get; set; }
+        public virtual Guid ItemId { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string ImageUrl { get; set; }
+        public virtual string Description { get; set; }
+        public virtual DateTime? StartDate { get; set; }
+        public virtual DateTime? EndDate { get; set; }
+        public virtual string RecurrenceId { get; set; }
+        public virtual string RecurrenceException { get; set; }
+        public virtual string RecurrenceRule { get; set; }
+        public virtual bool IsAllDay { get; set; }
+    }
+}
